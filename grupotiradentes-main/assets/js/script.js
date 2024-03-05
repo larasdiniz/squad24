d9b3ea6a96205e5aa60d4ef15632efaca39043ca
Lara Diniz <149534073+larasdiniz@users.noreply.github.com>
2024-03-05 14:34:31 -0300

    Add files via upload

---
+++ grupotiradentes-main/assets/js/script.js
@@ -0,0 +1,59 @@
+'use strict';
+
+
+
+/**
+ * addEvent no elemento
+ */
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+}
+
+
+
+/**
+ * alternância da barra de navegação
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const navbarToggler = document.querySelector("[data-nav-toggler]");
+
+const toggleNav = function () {
+  navbar.classList.toggle("active");
+  navbarToggler.classList.toggle("active");
+}
+
+addEventOnElem(navbarToggler, "click", toggleNav);
+
+const closeNav = function () {
+  navbar.classList.remove("active");
+  navbarToggler.classList.remove("active");
+}
+
+addEventOnElem(navbarLinks, "click", closeNav);
+
+
+
+/**
+ * cabeçalho ativo
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
